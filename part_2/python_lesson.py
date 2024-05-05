5d96a85718604ef7f4ca90617e4ca3c7b7f65c61
shohatl <lidorshohat@gmail.com>
2024-05-05 12:48:54 +0300

    pythagorean_triplet_by_sum impl done

--- part_2/python_lesson.py
+++ part_2/python_lesson.py
@@ -2,5 +2,17 @@ def num_len(num: int) -> int:
     return num_len(num=int(num / 10)) + 1 if num > 10 else 1
 
 
+def pythagorean_triplet_by_sum(sum: int):
+    if sum <= 0:
+        raise ValueError("sum must be bigger than 0")
+    for a in range(1, int(sum / 2) + 1):
+        for b in range(a, int((sum - a) / 2) + 1):
+            c = sum - b - a
+            if pow(c, 2) == pow(a, 2) + pow(b, 2):
+                nums = [a, b, c]
+                nums.sort()
+                print("<".join(map(str, nums)))
+
+
 if __name__ == '__main__':
-    print(num_len(123456))
+    pythagorean_triplet_by_sum(0)
