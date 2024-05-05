4c55d719961d0c01036a617a8499c48548bb0fb8
shohatl <lidorshohat@gmail.com>
2024-05-05 13:41:38 +0300

    question_4 impl done

--- part_2/python_lesson.py
+++ part_2/python_lesson.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+
 def num_len(num: int) -> int:
     return num_len(num=int(num / 10)) + 1 if num > 10 else 1
 
@@ -29,5 +32,34 @@ def is_sorted_polyndrom(string: str) -> bool:
     return True
 
 
+def question_4():
+    nums = []
+    sum = 0
+    positive_num = 0
+
+    while True:
+        num = float(input("Enter a num, -1 to end"))
+        if num == -1:
+            break
+        if num > 0:
+            positive_num += 1
+        sum += num
+        nums.append(num)
+
+    print(f"the average is:{sum / len(nums)}")
+    print(f"there are {positive_num} positive numbers")
+    print("you entered the numbers:")
+    sorted_nums = nums.copy()
+    sorted_nums.sort()
+    print(",".join(map(str, sorted_nums)))
+
+    # bonus questions
+    plt.plot(nums, "ro")
+    plt.show()
+
+    pearson_corr, _ = pearsonr(nums, range(len(nums)))
+    print("the pearson correlation coefficient is: ", pearson_corr)
+
+
 if __name__ == '__main__':
-    print(is_sorted_polyndrom("lbcdgdcbl"))
+    question_4()
