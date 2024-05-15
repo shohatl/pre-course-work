08dd8ac44c146878412939bd136a629b92270145
shohatl <lidorshohat@gmail.com>
2024-05-15 12:07:28 +0300

    7d ready

--- part_3/question_7.cs
+++ part_3/question_7.cs
@@ -3,22 +3,33 @@ namespace Question7
 
     public class NumericalExpression
     {
-        public int num { get; set; }
-        private List<string> first = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-        private List<string> second = new List<string> { "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-        private List<string> ten = new List<string> { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private int InitialNum;
+        public int Num { get; set; }
+        private readonly List<string> first = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private readonly List<string> second = new List<string> { "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        private readonly List<string> ten = new List<string> { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+        private readonly List<string> addons = new List<string> { "thousand", "million", "billion", "trillion", "quadrillion"};
+
         public NumericalExpression(int num)
         {
-            this.num = num;
+            this.Num = num;
+            this.InitialNum = num;
+        }
+
+        public int GetValue()
+        {
+            return this.InitialNum;
         }
 
         public override string ToString()
         {
-            string numberString = this.num.ToString();
+            string numberString = this.Num.ToString();
             while (numberString.Length % 3 != 0)
             {
                 numberString = "0" + numberString;
             }
+            int neededAddons = numberString.Length / 3 - 2;
             string final = "";
             for (int i = 0; i < numberString.Length; i++)
             {
@@ -30,11 +41,31 @@ namespace Question7
                     }
                     else if (i % 3 == 1)
                     {
-                        final += this.second[numberString[i] - '0' - 1] + " ";
+                        if(numberString[i] == '1')
+                        {
+                            i++;
+                            final += this.ten[numberString[i] - '0' - 1] + " ";
+                        }
+                        else{
+                            final += this.second[numberString[i] - '0' - 1] + " ";
+                        }
                     }
                     else if (i % 3 == 2)
                     {
                         final += this.first[numberString[i] - '0' - 1] + " ";
+
+                    }
+                      
+                    if(i % 3 == 2)
+                    {
+                        if (neededAddons > -1)
+                        {
+                            if (neededAddons < this.addons.Count)
+                            {
+                                final += this.addons[neededAddons] + ", ";
+                            }
+                            neededAddons--;
+                        } 
                     }
                 }
             }
@@ -47,7 +78,7 @@ namespace Question7
     {
         public static void Main()
         {
-            NumericalExpression num = new NumericalExpression(1234);
+            NumericalExpression num = new NumericalExpression(1212121214);
             Console.WriteLine(num.ToString());
         }
     }
